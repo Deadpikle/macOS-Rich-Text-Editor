92675954770ed4262a565635913570b443886b5c
Deadpikle <mikebobby@sbcglobal.net>
2018-03-28 12:13:35 -0400

    Add workspace and framework output

---
+++ macOSRichTextEditor/macOSRichTextEditor/macOSRichTextEditor.h
@@ -0,0 +1,19 @@
+//
+//  macOSRichTextEditor.h
+//  macOSRichTextEditor
+//
+//  Created by School of Computing Macbook on 3/28/18.
+//  Copyright © 2018 Pikle Productions. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for macOSRichTextEditor.
+FOUNDATION_EXPORT double macOSRichTextEditorVersionNumber;
+
+//! Project version string for macOSRichTextEditor.
+FOUNDATION_EXPORT const unsigned char macOSRichTextEditorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <macOSRichTextEditor/PublicHeader.h>
+
+
