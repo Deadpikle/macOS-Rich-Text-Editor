012eb54863bca4a1c19269f929814ab31a79ba0c
Deadpikle <mikebobby@sbcglobal.net>
2018-03-28 12:50:50 -0400

    Update umbrella header

--- macOSRichTextEditor/macOSRichTextEditor/macOSRichTextEditor.h
+++ macOSRichTextEditor/macOSRichTextEditor/macOSRichTextEditor.h
@@ -16,4 +16,6 @@ FOUNDATION_EXPORT const unsigned char macOSRichTextEditorVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <macOSRichTextEditor/PublicHeader.h>
 
-
+#include <macOSRichTextEditor/RichTextEditor.h>
+#include <macOSRichTextEditor/NSFont+RichTextEditor.h>
+#include <macOSRichTextEditor/NSAttributedString+RichTextEditor.h>
